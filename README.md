86df136a6078684d0d7cf82ac31657840a102f20
Zeitgeist <shecksk8nick@gmail.com>
2021-08-12 05:35:18 -0500

    Added csv support, added warnings, added Read Me

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # genetic-string-calculation
+
+This programm intends to reach a passed string by implementing genetic algorithms.
+
+The program can be used by executing:
+
+```javascript
+  node geneticString.js
+```
+
+You can pass your desired string by using the `stringGoal` flag, in case you don't provide a string 'Politecnico; Colombiano Jaime Isaza Cadavid' will be used as default
+
+```javascript
+  node geneticString.js --goalString 'My test string'
+```
+
+Additionally you can dismiss the parents log from your terminal by using the `noShow` flag, this log is displayed by default
+
+```javascript
+  node geneticString.js --noShow
+```
+
+The result will be saved into a csv file named `generationsResult.csv`
+
+Author:
+
+- Zeitgeist J
+- Santiago Quiceno
--- geneticString.js
+++ geneticString.js
@@ -1,7 +1,16 @@
+// eslint-disable-next-line no-undef
+const { stringGoal, noShow } = require('yargs')(process.argv.slice(2)).argv;
+const { parse } = require('json2csv');
+const fs = require('fs');
+require('console-warn');
+
+const fields = ['generations', 'parent1', 'parent2', 'value', 'score', 'hasMutated'];
 const math = Math;
-const goalString = 'Politecnico Colombiano Jaime Isaza Cadavid';
+const goalString = stringGoal || 'Politecnico; Colombiano Jaime Isaza Cadavid';
 const wordsLenght = goalString.length;
 const maxGenerations = 1000;
+
+const resultsArray = [];
 
 // population and population count
 const lettersPopulation = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghjiklmnopqrstuvwxyz,!? 0123456789'.split('');
@@ -65,6 +74,7 @@ const createInitialStringGeneration = () => {
   for (let i = 0; i < childrenPerGeneration; i += 1) {
     const randomString = goalString.split('').map(() => getRandomLetter()).join('');
     initialGeneration.push(randomString);
+    resultsArray.push({ generations, value: randomString, score: getWordScore(randomString) })
   }
 
   return initialGeneration;
@@ -76,6 +86,7 @@ const findBestsChildren = (generation) => {
   const sortedChildren = [...generation]
     .map((child) => ({ value: child, score: getWordScore(child) }))
     .sort((word1, word2) => word2.score - word1.score);
+
   const [bestChild, secondBestChild] = sortedChildren;
 
   return [bestChild, secondBestChild];
@@ -89,7 +100,10 @@ const updateParents = (generation) => {
 
   const comparedParentsWithNewChildren = [...parents, bestChild, secondBestChild].sort((child1, child2) => child2.score - child1.score);
 
-  console.table(comparedParentsWithNewChildren)
+  if (!noShow) {
+    console.table(comparedParentsWithNewChildren)
+  }
+
   parents = comparedParentsWithNewChildren.slice(0, 2);
 };
 
@@ -109,9 +123,8 @@ const mutate = (string) => {
 // returns new generation, params must be the current parents
 const crossParents = () => {
   const [firstParent, secondParent] = parents
-  console.log(firstParent, secondParent)
   increaseGenerationCount();
-  const nextGeneration = [['crossValue1', 'crossValue2', 'child', 'childMutated']]
+  const nextGeneration = [];
 
   for (let i = 0; i < childrenPerGeneration; i += 1) {
     const randomCrossValue = getRandomPosition();
@@ -124,17 +137,31 @@ const crossParents = () => {
 
     const crossedChild = crossedValue1 + crossedValue2;
     const mutatedChild = mutate(crossedChild);
-    nextGeneration.push([crossedValue1, crossedValue2, crossedChild, mutatedChild])
+    resultsArray.push({
+      generations,
+      parent1: parents[0].value,
+      parent2: parents[1].value,
+      value: mutatedChild,
+      score: getWordScore(mutatedChild),
+      hasMutated: crossedChild !== mutatedChild
+    });
+    nextGeneration.push(mutatedChild);
   }
 
-  return nextGeneration.slice(1).map((item) => item[3])
-  // return nextGeneration;
+  return nextGeneration;
 };
 
 // Stop criteria, return true when goalString was reached or then max generations have been created
 const shouldStop = () => ((generations === maxGenerations) || (parents[0].value === goalString));
 
 const main = () => {
+  const notInPopulation = filterGoalStringWithPopulation();
+
+  if (notInPopulation.length) {
+    console.warn('Some charaters in the goal string are not in the population');
+    console.warn('Possible memory leak and unreacheable response');
+  }
+
   const initialStringGeneration = createInitialStringGeneration();
 
   let currentGeneration = initialStringGeneration;
@@ -144,7 +171,11 @@ const main = () => {
     currentGeneration = crossParents(parents);
   } while (!shouldStop());
 
+  const csv = parse(resultsArray, { fields })
+  fs.writeFileSync('generationsResult.csv', csv);
+
   console.log('generations: ', generations);
+  console.log('bestResult: ', parents[0]);
 };
 
 main();
